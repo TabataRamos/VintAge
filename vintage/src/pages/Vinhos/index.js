53e02bea2ee83f6ad309463cf969ac45afead5d6
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-03-31 16:28:47 -0300

    consumo da API buscando todos vinhos

--- vintage/src/pages/Vinhos/index.js
+++ vintage/src/pages/Vinhos/index.js
@@ -2,17 +2,42 @@ import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
 import Body from "../../components/Body/Body";
 import Footer from "../../components/Footer/Footer";
+import { useEffect, useState } from "react";
+import { instance } from "../../services/api";
 
 function Vinhos() {
+  const [vinhos, setVinhos] = useState([]);
+
+  useEffect(() => {
+    async function getVinhos() {
+      try {
+        const response = await instance.get("/vinhos/lista");
+        setVinhos(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getVinhos();
+  });
+
   return (
     <>
       <div>
         <Header />
         <Banner imagem="nossosVinhos" logo="logo-hidden" />
       </div>
+      <Body></Body>
       <div>
-        <Body />
+        {vinhos.map((vinho) => {
+          return (
+            <div key={vinho.id}>
+              <img src={vinho.link_foto} alt={vinho.Uva.nome_uva}></img>
+              <h4>{vinho.Uva.nome_uva}</h4>
+            </div>
+          );
+        })}
       </div>
+
       <Footer />
     </>
   );
