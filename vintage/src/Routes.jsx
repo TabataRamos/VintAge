239f27c515f60737563883bf5763ee14002ce0f4
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-05 15:01:04 -0300

    Rota de vinhos por id adicionada

--- vintage/src/Routes.jsx
+++ vintage/src/Routes.jsx
@@ -1,13 +1,10 @@
-import {
-  BrowserRouter,
-  Routes as RoutesWrapper,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes as RoutesWrapper, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Contato from "./pages/Contato";
 import Vinicola from "./pages/Vinicola";
 import Vinhos from "./pages/Vinhos";
+import DetalheVinho from "./pages/DetalheVinho";
 
 function Routes() {
   return (
@@ -17,6 +14,7 @@ function Routes() {
         <Route path="/contato" element={<Contato />} />
         <Route path="/vinicola" element={<Vinicola />} />
         <Route path="/vinhos" element={<Vinhos />} />
+        <Route path="/vinhos/:id" element={<DetalheVinho />} />
       </RoutesWrapper>
     </BrowserRouter>
   );
