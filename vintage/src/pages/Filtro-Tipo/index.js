a5b5ffd3b9c1081989d0bf7a38de08861fc4b179
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-05 19:21:09 -0300

    base de consumo API do filtro de tipos

---
+++ vintage/src/pages/Filtro-Tipo/index.js
@@ -0,0 +1,52 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { instance } from "../../services/api";
+import Header from "../../components/Header/Header";
+import Banner from "../../components/Banner/Banner";
+import Body from "../../components/Body/Body";
+import Title from "../../components/Title/Title";
+import Line2 from "../../components/Line2/Line2";
+import Footer from "../../components/Footer/Footer";
+
+export default function FiltroTipo() {
+  const params = useParams();
+  const [vinhos, setVinho] = useState([]);
+
+  useEffect(() => {
+    async function getVinho() {
+      try {
+        const response = await instance.get(`/vinhos/${params.tipo}`);
+        setVinho(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getVinho();
+  }, [params]);
+  return (
+    <>
+      <div>
+        <Header />
+        <Banner imagem={params.tipo} logo="logo-hidden" />
+      </div>
+      <Body></Body>
+      <Title text={params.tipo} />
+      <Line2 />
+      <div className="__catalogo">
+        {vinhos.map((vinho) => {
+          return (
+            <div className="__vinho">
+              <div key={vinho.id}>
+                <img src={vinho.link_foto} alt={vinho.Uva.nome_uva}></img>
+                <h4>Vintage {vinho.Uva.nome_uva}</h4>
+                <p>{vinho.Tipo.nome_tipo}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <Footer />
+    </>
+  );
+}
