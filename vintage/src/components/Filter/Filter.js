3dca555cee3b0321fc05402f745f07bb4c685138
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-05 19:20:47 -0300

    link dos tipos adicionado

--- vintage/src/components/Filter/Filter.js
+++ vintage/src/components/Filter/Filter.js
@@ -10,26 +10,32 @@ function Filter(props) {
     <div>
       <Title text="Filtrar por tipo:"></Title>
       <div className="filtro-tipo">
-        <ButtonOpen
-          text="Tinto"
-          icon={<Icon icon="bxs:wine" color="#c03818" height="24" />}
-        ></ButtonOpen>
-        <ButtonOpen
-          text="Branco"
-          icon={
+        <Link to="/nossos-vinhos/tintos">
+          <ButtonOpen
+            text="Tinto"
+            icon={<Icon icon="bxs:wine" color="#c03818" height="24" />}
+          ></ButtonOpen>
+        </Link>
+        <Link to="/nossos-vinhos/brancos">
+          <ButtonOpen
+            text="Branco"
+            icon={<Icon icon="fa6-solid:wine-glass" color="#f6db8e" height="24" />}
+          >
             <Icon icon="fa6-solid:wine-glass" color="#f6db8e" height="24" />
-          }
-        >
-          <Icon icon="fa6-solid:wine-glass" color="#f6db8e" height="24" />
-        </ButtonOpen>
-        <ButtonOpen
-          text="Rosé"
-          icon={<Icon icon="mdi:glass-wine" color="#f68e8e" height="24" />}
-        ></ButtonOpen>
-        <ButtonOpen
-          text="Espumante"
-          icon={<Icon icon="mdi:glass-flute" color="#fff3d0" height="24" />}
-        ></ButtonOpen>
+          </ButtonOpen>
+        </Link>
+        <Link to="/nossos-vinhos/roses">
+          <ButtonOpen
+            text="Rosé"
+            icon={<Icon icon="mdi:glass-wine" color="#f68e8e" height="24" />}
+          ></ButtonOpen>
+        </Link>
+        <Link to="/nossos-vinhos/espumantes">
+          <ButtonOpen
+            text="Espumante"
+            icon={<Icon icon="mdi:glass-flute" color="#fff3d0" height="24" />}
+          ></ButtonOpen>
+        </Link>
       </div>
       <div className="filtro-tipo">
         <Link to="/nossos-vinhos/todos">
