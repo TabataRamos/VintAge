15359bded71aef1100760a23ee8f13b8bab8794e
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-06 22:57:10 -0300

    ícones harmonizações

--- vintage/src/pages/DetalheVinho/index.js
+++ vintage/src/pages/DetalheVinho/index.js
@@ -6,11 +6,28 @@ import Header from "../../components/Header/Header";
 import Body from "../../components/Body/Body";
 import Line2 from "../../components/Line2/Line2";
 import Footer from "../../components/Footer/Footer";
+import { Icon } from "@iconify/react";
 
 export default function DetalheVinho() {
   const params = useParams();
   const [vinhos, setVinho] = useState([]);
-
+  const harmonizacoes = {
+    1: "tabler:meat",
+    2: "icon-park-outline:chicken-leg",
+    3: "tabler:meat",
+    4: "tabler:meat",
+    5: "tabler:meat",
+    7: "tabler:sausage",
+    8: "bx:cheese",
+    9: "bx:cheese",
+    10: "mdi:pasta",
+    11: "la:pepper-hot",
+    12: "ion:fish-outline",
+    13: "fa6-solid:shrimp",
+    14: "tabler:mushroom",
+    15: "tabler:mushroom",
+    17: "ri:cake-3-line",
+  };
   useEffect(() => {
     async function getVinho() {
       try {
@@ -30,12 +47,27 @@ export default function DetalheVinho() {
         <Body></Body>
         <Line2 lineNumber="line-100" />
       </div>
-      <div className="__catalogo">
+      <div>
         {vinhos.map((vinho) => {
           return (
-            <div className="__vinho">
+            <div>
               <div key={vinho.id}>
                 <img src={vinho.link_foto} alt={vinho.Uva.nome_uva}></img>
+                <h5>Descrição:</h5>
+                <p>{vinho.descricao}</p>
+                <h4>Harmonizações:</h4>
+                {vinho.Pratos.map((prato) => {
+                  return (
+                    <>
+                      <Icon
+                        icon={harmonizacoes[prato.Vinhos_Pratos.prato_id]}
+                        color="#f6db8e"
+                        height="60"
+                      />
+                      <h5>{prato.nome_prato}</h5>
+                    </>
+                  );
+                })}
                 <h4>Vintage {vinho.Uva.nome_uva}</h4>
                 <p>{vinho.Tipo.nome_tipo}</p>
               </div>
