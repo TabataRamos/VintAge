a8043cee3c6143152f605152fbd21682695c6392
TabataRamos <tabata.b.ramos@gmail.com>
2022-04-07 23:44:35 -0300

    responsividade para celulares

--- vintage/src/components/BodyVinicola/BodyVInicola.js
+++ vintage/src/components/BodyVinicola/BodyVInicola.js
@@ -9,27 +9,32 @@ function BodyVinicola() {
       <div className="vinicola-inicio">
         <Title text="A Vinícola"></Title>
         <h2>
-          A vinícola Vintage está localizada na região serrana de Santa Catarina, no <br />{" "}
-          município de São Joaquim.
+          A vinícola Vintage está localizada na região serrana de Santa
+          Catarina, no <br /> município de São Joaquim.
         </h2>
         <Line2 lineNumber="line-50"></Line2>
       </div>
       <div className="vinicola-info">
-        <img src={require("../../assets/images/vinicola-1.png")} alt="Foto da Vinícola"></img>
+        <img
+          src={require("../../assets/images/vinicola-1.png")}
+          alt="Foto da Vinícola"
+        ></img>
 
         <p>
-          Ao norte dos vinhedos está o casarão. A antiga casa da família foi tombada como patrimônio
-          histórico do estado de Santa Catarina em 2016. Essa belíssima construção em pedra,
-          pertence à família há 12 gerações e guarda memórias de nossos descendentes imigrantes
-          italianos. Mais que o DNA, o amor pela vitivinicultura de nossos antepassados foi passado
-          de geração em geração.
+          Ao norte dos vinhedos está o casarão. A antiga casa da família foi
+          tombada como patrimônio histórico do estado de Santa Catarina em 2016.
+          Essa belíssima construção em pedra, pertence à família há 12 gerações
+          e guarda memórias de nossos descendentes imigrantes italianos. Mais
+          que o DNA, o amor pela vitivinicultura de nossos antepassados foi
+          passado de geração em geração.
         </p>
       </div>
       <Line2 lineNumber="line-60-end"></Line2>
       <div className="vinicola-info-dois">
         <p>
-          Nossos barris de maturação ficam em uma construção subterrânea que se estende desde o
-          porão da casa da família até próximo a estrada dos vinhedos.
+          Nossos barris de maturação ficam em uma construção subterrânea que se
+          estende desde o porão da casa da família até próximo a estrada dos
+          vinhedos.
         </p>
         <img
           className="img-end"
@@ -41,16 +46,19 @@ function BodyVinicola() {
       <div className="visitacao">
         <h1>Visitação</h1>
         <div className="visitacao-container-um">
-          <img src={require("../../assets/images/vinicola-3.png")} alt="Foto da Vinícola"></img>
+          <img
+            src={require("../../assets/images/vinicola-3.png")}
+            alt="Foto da Vinícola"
+          ></img>
 
           <div>
             <p className="container-um">
-              Que tal observar um lindo por-do-sol enquanto aprecia um bom vinho?
+              Que tal observar um lindo por-do-sol enquanto aprecia um bom
+              vinho?
             </p>
             <p className="container-dois">
-              Nosso salão de jantar e jardim com <br />
-              vista para o vinhedo estão abertos <br />
-              para visitação agendada.
+              Nosso salão de jantar e jardim com vista para o vinhedo estão
+              abertos para visitação agendada.
             </p>
             <a href="#scroll">
               <button className="button-agende">
@@ -72,7 +80,6 @@ function BodyVinicola() {
           <h1>Eventos</h1>
           <h2>
             Sazonalmente nós promovemos eventos de degustação e cursos para
-            <br />
             amantes de vinhos e futuros produtores.
           </h2>
         </div>
